16763bc7fd562edafa066158ab400790dbb0beec
KKJ <39583312+kkjsw17@users.noreply.github.com>
2022-03-10 16:05:02 +0900

    fix : word 파일 send 후 바로 삭제되도록 구현

--- controllers/py-controller.js
+++ controllers/py-controller.js
@@ -1,5 +1,6 @@
 const spawn = require('child_process').spawn;
 var path = require('path');
+var fs = require('fs');
 
 exports.generateDocx = (req, res) => {
     const { meeting, report } = req.query;
@@ -9,7 +10,13 @@ exports.generateDocx = (req, res) => {
         const filename = `${result.toString().replace(/\n/g, "")}.docx`;  
         res.setHeader('Content-Disposition', `attachment; filename=${filename}`); // 이게 핵심 
         res.setHeader('Content-Type', '"application/octet-stream"');
-        res.sendFile(path.join(__dirname, '../', filename));
+        res.sendFile(path.join(__dirname, '../', filename), function() {
+            fs.unlink(path.join(__dirname, '../', filename), err => {
+                if (err) {
+                    console.log("Error : ", err)
+                }
+            });
+        });
     });
 
     python.stderr.on('data', function(data) {

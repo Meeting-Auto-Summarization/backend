70241c60d0b46d1156ad3ba5da1eda60cf34353a
geonjunk <koeunjun7@gmail.com>
2022-11-03 03:06:59 +0900

    Refactoring : Callback URL 수정

--- was/passport/googleStrategy.js
+++ was/passport/googleStrategy.js
@@ -8,7 +8,7 @@ module.exports = () => {
     passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: `${process.env.CALLBACK_URL}/auth/google/callback`,
+        callbackURL: process.env.CALLBACK_URL,
     }, async (accessToken, refreshToken, profile, done) => {
         console.log('google profile', profile);
         try {
